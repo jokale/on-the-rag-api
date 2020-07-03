e2ac0c3e98be8f0b1f5699bb656e99e02c63bd8e
jokale <kalemajoanna@gmail.com>
2020-07-03 18:16:23 +0100

    add user auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, only: [:create, :show, :index]
   resources :notes
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
   post 'user_token' => 'user_token#create'
   post 'find_user' => 'users#find'
   resources :users do
